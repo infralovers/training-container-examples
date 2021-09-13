29fa0a5b7d3776d1a207e75a990e23bb6a494f30
Jürgen Brüder <juergen.brueder@gmail.com>
2021-09-13 14:16:17 +0200

    make environment name a replaceable variable

--- replace_variables.sh
+++ replace_variables.sh
@@ -5,3 +5,6 @@ find 12* a* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
 
 # Replace <ENV-ANIMAL>
 find 12* a* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
+
+# Replace <ENV-NAME>
+find 12* a* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
